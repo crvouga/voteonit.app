cbbddbcc999fc3faceb0cabc21e546bd0209df2c
Chris Vouga <crvouga@gmail.com>
2023-12-30 15:27:03 -0700

    Refactor code for improved performance

---
+++ src/main/client.cljs
@@ -0,0 +1,5 @@
+(ns client)
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn init []
+  (println "Hello World"))
---
+++ src/main/server.cljs
@@ -0,0 +1,5 @@
+(ns server)
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn init []
+  (println "Hello World"))
