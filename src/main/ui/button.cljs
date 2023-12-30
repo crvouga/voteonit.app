e53de802bcea37e98d71aaddd98054172ac43ffa
Chris Vouga <crvouga@gmail.com>
2023-12-30 16:52:10 -0700

    Update button styling to include hover and active states

--- src/main/ui/button.cljs
+++ src/main/ui/button.cljs
@@ -4,6 +4,6 @@
 
 (defmethod view :contained
   [input]
-  [:button.p-3.text-lg.font-bold.bg-blue-500.rounded.text-white input (:text input)])
+  [:button.active:opacity-60.hover:opacity-90.p-3.text-lg.font-bold.bg-blue-500.rounded.text-white.ring-offset-1 input (:text input)])
 
 (defmethod view :default [input] (view (assoc input :variant :contained)))
