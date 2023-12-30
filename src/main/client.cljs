111330dd48e5701ccfc43aee7dca717f4102149a
Chris Vouga <crvouga@gmail.com>
2023-12-30 16:00:09 -0700

    Update dependencies and add React components

--- src/main/client.cljs
+++ src/main/client.cljs
@@ -1,5 +1,20 @@
-(ns client)
+(ns client
+  (:require [reagent.dom :as rd] 
+            [reagent.core :as r]))
+
+(def state (r/atom {:count 0}))
+
+(defn- increment! []
+  (swap! state update :count inc))
+
+(defn- hello-world []
+  [:ul
+   [:li "Hello"]
+   [:button {:on-click increment!} (str "Count: " (-> @state :count))]
+   [:li {:style {:color "red"}} "World!"]])
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn init []
-  (println "Hello World"))
+  (println "Hello World")
+  (rd/render [hello-world] (js/document.getElementById "root")))
+
