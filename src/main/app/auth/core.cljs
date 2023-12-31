fb285a5a8787b27dff9b798beac576f9a1530e1d
Chris Vouga <crvouga@gmail.com>
2023-12-30 18:57:16 -0700

    Add new email-related functions and update server initialization

--- src/main/app/auth/core.cljs
+++ src/main/app/auth/core.cljs
@@ -1,3 +1,5 @@
 (ns app.auth.core)
 
 (def user-clicked-send-login-link-email ::user-clicked-send-login-link-email)
+(def sent-login-link-email ::sent-login-link-email)
+(def send-login-link-email-errored ::send-login-link-email-errored)
--- src/main/app/auth/server.cljs
+++ src/main/app/auth/server.cljs
@@ -3,8 +3,27 @@
             [app.auth.core]))
 
 (defn init []
-  {::sessions {}})
+  {::session-id-by-client-id {}
+   ::user-id-by-session-id {}
+   ::session-ids #{}
+   ::accounts-by-user-id {}})
+
+(defn send-email
+  [input {:keys [email]}]
+  (println "send email to " email))
+
+(defn make-login-email [email]
+  (str "https://voteonit.app/login/" email))
+
+(defn send-to-client [input to-client]
+  (let [eff (fn [] (println "todo" to-client))
+        output (core/add-effect input eff)]
+    output))
 
 (defmethod step app.auth.core/user-clicked-send-login-link-email [input]
-  (println "server step" input)
-  input)
+  (let [email (-> input :msg :email)
+        login-link-email (make-login-email email)
+        sent-email (send-email input login-link-email)]
+    sent-email))
+
+
