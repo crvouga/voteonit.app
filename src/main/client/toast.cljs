ce8ff0a3b668f9f3d0eae8722f1dbe790b959061
Chris Vouga <crvouga@gmail.com>
2023-12-31 17:22:37 -0700

    Refactor toast.cljs file

--- src/main/client/toast.cljs
+++ src/main/client/toast.cljs
@@ -2,8 +2,22 @@
   (:require [core :refer [handle-command append-command handle-msg]]
             [cljs.core.async :refer [go-loop timeout <!]]))
 
+;; 
+;; 
+;; 
+;; State
+;; 
+;; 
+;; 
+
 (defn initial-state []
   {::toast nil})
+
+;; 
+;; 
+;; Toast
+;; 
+;; 
 
 (defn new-toast [message]
   {:message message
@@ -17,6 +31,12 @@
         elapsed (- now created-at)]
     (>= elapsed duration)))
 
+;; 
+;; 
+;; Command
+;; 
+;; 
+
 (defmethod handle-command ::show-toast [input] 
   (let [message (-> input :command :message)
         toast (new-toast message)]
@@ -26,19 +46,39 @@
 (defn show-toast [input message]
   (append-command input {:type ::show-toast :message message}))
 
+;; 
+;; 
+;; Msg
+;; 
+;; 
+
 (defmethod handle-msg ::time-passed [input]
   (let [toast (-> input :state ::toast)
         removed (assoc-in input [:state ::toast] nil)
         output (if (toast-expired? toast) removed input)]
     output))
 
+;; 
+;; 
+;; View
+;; 
+;; 
+
+
 
 (defn view [{:keys [state]}]
   (let [message (-> state ::toast :message)]
      (when message
        [:div.absolute.inset-0.flex.items-start.justify-center.pointer-events-none.p-4
-        [:div.w-full.px-4.p-2.text-white.bg-neutral-700.rounded.text-base.font-semibold
+        [:div.w-full.px-4.p-2.text-white.bg-neutral-700.rounded.text-base.font-semibold.transition-all
+         
          message]])))
+
+;; 
+;; 
+;; Subscriptions
+;; 
+;; 
 
 (defn subscriptions! [state! dispatch!]
   (go-loop []
