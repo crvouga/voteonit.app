f0514fde00744d3d5825ba55a623c85c2950659a
Chris Vouga <crvouga@gmail.com>
2024-01-07 17:50:44 -0700

    Update datascript_db.edn and db.cljs

--- src/main/db.cljs
+++ src/main/db.cljs
@@ -4,28 +4,5 @@
             [db.storage.filesystem]
             [datascript.core :as d]))
 
-
-(def empty-db (d/empty-db db.schema/schema))
-
-#_(defn ensure-db [db]
-  (if (d/db? db) db empty-db))
-
-#_(def db-stored 
-  (ensure-db (db.storage/read-db! {})))
-
 (def conn! 
-  (d/conn-from-db empty-db))
-
-(d/transact! conn! [{:db/ident :db/schema 
-                     :db.schema/schema db.schema/schema}])
-
-(db.storage/write-db! {:conn! conn!})
-
-(defn on-shutdown []
-  (db.storage/write-db! {:conn! conn!})
-  (js/process.exit 0))
-
-(do
-  (.on js/process "SIGINT" on-shutdown)   
-  (.on js/process "SIGTERM" on-shutdown)  
-  (.on js/process "SIGQUIT" on-shutdown)) 
+  (d/create-conn db.schema/schema))
